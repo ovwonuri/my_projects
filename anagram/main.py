895c109acf94a7b04530d2d54608a7eb71a4ac8b
Abraham Oghenemega Ovwonuri <ovwonuri@gmail.com>
2022-05-26 14:32:44 +0100

    Add files via upload

--- anagram/main.py
+++ anagram/main.py
@@ -1,1 +1,21 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
 
+
+def find_anagrams(word, anagram):
+    # [assignment] Add your code here
+    word_list = list(word)
+    word_list.sort()
+    anagram_list = list(anagram)
+    anagram_list.sort()
+
+    if word_list == anagram_list:
+        return True
+    else:
+        return False
+
+
+# print(find_anagrams("hello", "check"))
+print(find_anagrams("below", "elbow"))
